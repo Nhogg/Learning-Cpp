6641fa81cea108341b6cf0330142e2598286e9e7
nhogg <nathanhogg1223@gmail.com>
2025-08-10 15:39:15 -0400

    Intro.cpp: Hello World program objects.cpp: introduction to objects

---
+++ intro/intro.cpp
@@ -0,0 +1,12 @@
+// Preprocessor directive
+// Indicates that we want to include functions
+// from the iostream library in our code
+#include <iostream>
+
+// Function definition
+int main()
+{
+	// Character output
+	std::cout << "Hello, world!";
+	return 0;
+}
